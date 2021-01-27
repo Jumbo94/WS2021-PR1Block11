07abb4100a3e4ed987da32c0584c0d7e529e0e73
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2021-01-27 19:18:25 +0100

    Palindrom erste Lösung

--- src/palindrom/DemoPalindrom.java
+++ src/palindrom/DemoPalindrom.java
@@ -7,14 +7,20 @@ public class DemoPalindrom {
     public static void main(String[] args) {
 
         System.out.println("isPalindrom(\"Otto\") = " + isPalindrom("Otto".toLowerCase()));
+        System.out.println("isPalindrom(\"Eine Horde bedrohe nie!\") = " + isPalindrom("Eine Horde bedrohe nie!".toLowerCase()));
     }
     
     public static boolean isPalindrom(String text) {
         System.out.println("text = " + text);
-
-
         // Ihr Code
+        if (text.length() <= 1)
+            return true;
 
-        return false;
+        if (text.charAt(0) == text.charAt(text.length()-1)){
+            // geht's weiter
+            return isPalindrom(text.substring(1, text.length() - 1));
+        }
+        else
+            return false;
     }
 }
